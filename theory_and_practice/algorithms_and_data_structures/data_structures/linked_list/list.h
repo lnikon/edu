7f5148ba6306dfb0f85efcb3a877594d861eff96
lnikon <lnikon@mail.ru>
2020-02-19 17:19:16 +0400

    wrkg on list

--- theory_and_practice/algorithms_and_data_structures/data_structures/linked_list/list.h
+++ theory_and_practice/algorithms_and_data_structures/data_structures/linked_list/list.h
@@ -1,1 +1,22 @@
 #pragma once
+
+struct list_elem_t
+{
+    list_elem_t* next;
+    void* data;
+};
+
+struct list_t
+{
+    list_elem_t* head;
+
+    /*
+     * 0 - equal
+     * 1 - first is greater
+     * -1 - first is less
+     */
+    void (*match)(const void* key1, const void* key2);
+    void (*destroy)(void* data);
+};
+
+void init_list()
